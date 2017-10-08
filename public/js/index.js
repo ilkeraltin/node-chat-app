44e80009a9b803ebe7ff3867e233b6079e4ed87d
ilker ALTIN <ilkerobi@gmail.com>
2017-10-08 13:26:46 +0300

    Greet new users and inform others added

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 var socket = io();
 socket.on('connect', function () {
     console.log('connected to server');
-});
+}); 
 
 socket.on('disconnect', function () {
     console.log('disconnected from server!!');
--- server/server.js
+++ server/server.js
@@ -14,13 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from:'Admin',
+        text: 'Welcome to the ChatApp'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined!',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
+     /*    io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        });
+        }); */
+
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        }); 
     });
 
     socket.on('disconnect', () => {
