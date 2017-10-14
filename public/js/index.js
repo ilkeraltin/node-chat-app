b2f7a78f7f40205918e0fa62bc153345dd7cbfaf
ilker ALTIN <ilkerobi@gmail.com>
2017-10-14 23:23:58 +0300

    scroll to bottom if user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket = io();
+function scrollToBottom() {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
 socket.on('connect', function () {
     console.log('connected to server');
 }); 
@@ -16,6 +30,7 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 })
 
 socket.on('newLocationMessage', function(message) {
@@ -29,6 +44,7 @@ socket.on('newLocationMessage', function(message) {
     })
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 })
 
 
